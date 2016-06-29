74f8b31a501f764116c34fa9c680c6e53e18b304
sikora507 <sikora507@gmail.com>
2016-06-29 20:56:12 +0200

    wpf 3d engine

--- CarmaBrowser/UiComponents/Cars/CarsView.xaml.cs
+++ CarmaBrowser/UiComponents/Cars/CarsView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using System.Windows.Media.Media3D;
 
 namespace CarmaBrowser.UiComponents
 {
@@ -23,6 +11,31 @@ namespace CarmaBrowser.UiComponents
         public CarsView()
         {
             InitializeComponent();
+            //// front face
+            //_mesh.Positions.Add(new Point3D(-10, 0, 0));
+            //_mesh.Positions.Add(new Point3D(10, 0, 0));
+            //_mesh.Positions.Add(new Point3D(-10, 20, 0));
+            //_mesh.Positions.Add(new Point3D(10, 20, 0));
+            //// up face
+            //_mesh.Positions.Add(new Point3D(-10, 20, -20));
+            //_mesh.Positions.Add(new Point3D(-10, 20, 0));
+            //_mesh.Positions.Add(new Point3D(10, 20, 0));
+            //_mesh.Positions.Add(new Point3D(10, 20, -20));
+
+            //// front face (counter clockwise)
+            //_mesh.TriangleIndices.Add(0);
+            //_mesh.TriangleIndices.Add(1);
+            //_mesh.TriangleIndices.Add(2);
+            //_mesh.TriangleIndices.Add(3);
+            //_mesh.TriangleIndices.Add(2);
+            //_mesh.TriangleIndices.Add(1);
+            //// up face
+            //_mesh.TriangleIndices.Add(4);
+            //_mesh.TriangleIndices.Add(5);
+            //_mesh.TriangleIndices.Add(6);
+            //_mesh.TriangleIndices.Add(7);
+            //_mesh.TriangleIndices.Add(6);
+            //_mesh.TriangleIndices.Add(5);
         }
     }
 }
